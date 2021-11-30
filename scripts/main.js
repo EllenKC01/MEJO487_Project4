9ae7c97b17b212f5681f7c1ec4b217f563565c44
EllenKC01 <ellen.k.cochran@gmail.com>
2021-11-30 03:12:39 -0500

    more edits

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,60 @@
+$(document).ready(function () {
+  console.log("ready");
+});
+
 let map;
 
+// create map
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 39.32, lng: -76.55 },
-    zoom: 5.3,
+    center: { lat: 39.32, lng: -77.55 },
+    zoom: 5.5,
+    mapTypeId: "terrain",
+  });
+
+  loadData();
+}
+
+// load json data
+function loadData() {
+  $.getJSON("./data/markers.json", function (places) {
+    console.log(places);
+    createMarkers(places);
+    createTable(places);
+  });
+}
+
+// create map markers w json data
+function createMarkers(places) {
+  var pos = "";
+  var title = "";
+  for (let i = 0; i <= places.length; i++) {
+    pos = { lat: places[i].lat, lng: places[i].lng };
+    console.log(pos);
+    title = places[i].name;
+    console.log(title);
+    new google.maps.Marker({
+      position: pos,
+      map: map,
+      title: title,
+    });
+  }
+
+  const marker = google.maps.Marker;
+
+  marker.addListener("click", () => {
+    map.setZoom(3);
+  });
+}
+
+function createTable(places) {
+  $("#myTable").DataTable({
+    data: places.data,
+    columns: [
+      { data: "image" },
+      { data: "state" },
+      { data: "name" },
+      { data: "description" },
+    ],
   });
 }
